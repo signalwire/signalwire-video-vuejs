546f079c6bba411d47aaea5791b33cad037d5147
virgilvox <mzara1@asu.edu>
2022-03-25 14:12:35 -0700

    fix room previews

--- frontend/src/components/ListRooms.vue
+++ frontend/src/components/ListRooms.vue
@@ -2,25 +2,19 @@
   <div class="listRooms" v-if="rooms" >
     <div class="row">
       <div class="room" v-for="room in rooms" :key="room.id">
-        <va-card>
-          <img 
-            v-if="!room.preview_url"
-            src="../assets/signalwire-full.png"
-            style="width: 300px;"
-          />
-          <div class="room-preview" v-if="room.preview_url">
-            <video class="room-preview-video" autoplay muted playsinline loop   
-              :src="room.preview_url" type="video/mp4"
-              
-            ></video>
-          </div>
-          <va-card-title>{{room.display_name}}</va-card-title>
-          <va-card-content>
-            <va-button v-on:click="joinRoom(room.name)" >
-              Join
-            </va-button>
-          </va-card-content>
-        </va-card>
+        <RoomCard
+          v-if="room.preview_url"
+          :preview_url="room.preview_url"
+          :display_name="room.display_name"
+          :room_name="room.name"
+        />
+
+        <RoomCard
+          v-if="!room.preview_url"
+          :preview_url="defaultPreview"
+          :display_name="room.display_name"
+          :room_name="room.name"
+        />
       </div>
     </div>
 
@@ -28,22 +22,24 @@
 </template>
 
 <script>
+import RoomCard from './RoomCard.vue'
 const helper = require('../api-helper')
 
 export default {
   name: 'ListRooms',
+  components: {
+    RoomCard
+  },
   data() {
     return {
-      rooms: null
+      rooms: null,
+      defaultPreview: 'https://signalwire-video-vuejs-frontend2.virgilvox.repl.co/img/signalwire-full.6adefa05.png'
     }
   },
   async mounted() {
     this.listRooms()
   },
   methods: {
-    joinRoom(roomname) {
-      window.location.href = `/in-call?roomname=${roomname}`
-    },
     async listRooms(){
       let rooms = await helper.listActiveRoomSessions()
       console.log(rooms)
@@ -63,22 +59,5 @@ export default {
   margin: auto;
 }
 
-.room {
-  width: 200;
-  margin: 10px;
-  align-content: center;
-  justify-content: center;
-}
-
-.room-preview {
-  overflow: hidden;
-  position: relative;
-  width: 300px;
-}
-
-.room-preview-video {
-  height:inherit;
-  width:inherit;
-}
 
 </style>
---
+++ frontend/src/components/RoomCard.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="roomCard">
+    <va-card v-on:click="joinRoom(room_name)">
+        <div class="room-preview" v-if="preview_url">
+        <img 
+          class="room-preview-video"
+          :src="preview_url"
+        />
+        </div>
+        <va-card-title>{{display_name}}</va-card-title>
+    </va-card>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'RoomCard',
+  props: {
+    preview_url: String,
+    display_name: String,
+    room_name: String
+  },
+  methods: {
+    joinRoom(roomname) {
+      window.location.href = `/in-call?roomname=${roomname}`
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+
+.roomCard {
+  width: 200px;
+  margin: 10px; 
+  align-content: center;
+  justify-content: center;
+  cursor: pointer;
+}
+
+.room-preview {
+  overflow: hidden;
+  position: relative;
+  width: inherit;
+}
+
+.room-preview-video {
+  height:inherit;
+  width:inherit;
+}
+
+</style>
